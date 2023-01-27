aa13a9e79f002f736cd3529e9183180a045b8a5a
Andersama <anderson.john.alexander@gmail.com>
2023-01-27 05:01:29 -0800

    Add licenses

--- containers/stable_stack.h
+++ containers/stable_stack.h
@@ -5,10 +5,35 @@
 #include <memory>
 #include <vector>
 
+/*
+The MIT License (MIT)
+
+Copyright (c) 2022 Alex Anderson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 // stable stack: maintains that pointers to it's internal members are stable as the stack grows
 //  warning: this implementation constructs entire blocks of memory and destructs all at once
 //  pop_back() and pop() do not destruct objects in memory, be wary of stale references and accesses
 //  because those objects will still be live
+
 template <typename T, size_t N = 32> struct stable_stack {
   public:
 	using array_type = std::array<T, N>;
